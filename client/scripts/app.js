f506c3ab288fbd07e984334b93fa8b3f5057eb46
Matt Bankston <mmbankston@icloud.com>
2015-07-29 18:57:37 -0500

    Added resume

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    $("#masterresume").hide("#masterresume")
     $('body').append("<button id='clickyButton'>Click me!</button>");
 
-    $("body").on('click','#clickyButton', function(){
-        $.ajax({
-            url: "/data-request",
-            success: function(data){
-                console.log(data);
-            }
-        })
-    });
+    $("body").on('click', '#clickyButton', function () {
+        $('#clickyButton').hide();
+        $("#masterresume").hide("#masterresume").slideDown("slow");
+
+    })
+    $("body").on('click', '#masterresume', function(){
+        $("#masterresume").hide("#masterresume").slideUp("slow");
+        $('#clickyButton').show();
+    })
 });
